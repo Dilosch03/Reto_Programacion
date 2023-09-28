fab0c26f05a1ec24fce05390964d6ec22fe5a8ae
Dilosch03 <71047126+Dilosch03@users.noreply.github.com>
2023-09-28 00:24:20 -0600

    Generacion de ventana

--- game.py
+++ game.py
@@ -1,7 +1,32 @@
 import numpy as np
 import pygame as pg
+import os
 
-tamaño_tablero = [5,5]
-tablero = []
 
-pygame.Overlay.display()
+ALTURA = 500
+LARGO = 900
+WINDOW = pg.display.set_mode((LARGO,ALTURA))
+FPS = 60
+
+def darw_event():
+    WINDOW.fill((40,40,240))
+
+def main():
+    clock = pg.time.Clock()
+    TAMAÑO_TABLERO = [5,5]
+    tablero = []   
+
+    runinng = True
+
+    while runinng:
+        clock.tick(FPS)
+        for event in pg.event.get():
+            match event.type:
+                case pg.QUIT:
+                    runinng = False
+
+    
+    pg.quit()
+
+if __name__ == "__main__":
+    main()
