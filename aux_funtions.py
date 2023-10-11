c8e65195c15a1fe8bc40f11fcf7f2e1157d0c441
Dilosch03 <71047126+Dilosch03@users.noreply.github.com>
2023-10-11 12:34:30 -0600

    jeopardy funcional

---
+++ aux_funtions.py
@@ -0,0 +1,271 @@
+import csv
+import os
+import pygame as pg
+import random as rd
+import tkinter as tk
+
+def string_loder(Tablero_size):
+    question_matrix = []
+    questionare_matrix = []
+    answer_matrix = []
+    headers = []
+    errores = []
+    
+    MAIN_PATH = os.path.dirname(__file__)
+    FILE = "jeopardy_questions.csv"
+    PATH = os.path.join(MAIN_PATH,FILE)
+
+    if not(os.path.isfile(PATH)):
+        return "err","err","err", ["Archivo de preguntas no se encuentra."]
+
+    with open(PATH, newline='') as csvfile:
+        has_header = csv.Sniffer().has_header(csvfile.read(1024))
+        csvfile.seek(0)  
+        reader = csv.reader(csvfile)
+
+        if has_header:
+            next(reader)  
+        
+        num = 0
+        flag = True
+
+        for row in reader:
+            flag = False
+            if len(row)==0:
+                continue
+
+            if row[0] in headers:
+                index =  int(headers.index(row[0]))
+                
+            else:
+                headers.append(row[0])
+                questionare_matrix.append([])
+                index = int(len(headers)-1.)
+            
+            err = False
+            
+            for element in row:
+                if (element==""):
+                    
+                    err = True
+            
+            if err:
+                num+=1
+                continue
+
+
+            questionare_matrix[index].insert(0,row)
+        
+        if flag:
+            errores.append("Archivo no contiene preguntas.")
+
+        if(num>0):
+            errores.append(str(num)+" preguntas no tienen todas las areas con datos.")
+
+    not_valid = []
+    counter = [0,0,0,0]
+    for i,theme in enumerate(questionare_matrix):
+        _temp = [[],[],[],[],[]]
+
+        if not(len(theme)>=Tablero_size[1]):
+            not_valid.append(i)
+            continue
+
+        for question in theme:
+            if not(len(question)==11):
+                counter[0] += 1
+                continue
+            if not(question[10].isnumeric()):
+                counter[1] += 1
+                continue
+            points = int(question[10])//100
+            if not(points < 6 and points>0):
+                counter[2] += 1
+                continue
+            index = (points)-1
+            _temp[index].insert(0,question)
+        
+        err = False
+
+        for l,points in enumerate(_temp):
+            if(len(points)<1):
+                err = True
+            rd.shuffle(points)
+            _temp[l] = points
+
+        if err:
+            counter[3] += 1
+            not_valid.append(i)
+            continue
+
+        questionare_matrix[i] = _temp
+
+    caracteristica = [" el tamaño adecuado."," un numero en el area de puntos."," un valor dentro de rango para los puntos."," temas no tienen pregntas para todas las categorias de puntos."]
+    for l,i in enumerate(counter):
+        if i>0:
+            text = " preguntas no tienen" + caracteristica[l]
+            if (l == 3):
+                text = caracteristica[l]
+            
+            errores.append(str(i)+text)
+
+    not_valid.reverse()
+    if len(not_valid)>0:
+        errores.append(str(len(not_valid))+" temas no cumplen con el numero minimo de preguntas.")
+
+    for i in not_valid:
+        del questionare_matrix[i]
+
+    if not(len(questionare_matrix)>=Tablero_size[0]):
+        errores.append("Numero de temas aceptados es menor al tamaño del tablero.")
+        return "err","err","err", errores
+    
+          
+    for i in range(len(questionare_matrix)):
+        answer_matrix.append(i)
+    
+    rd.shuffle(answer_matrix)
+
+    index = answer_matrix[0:Tablero_size[0]]
+    answer_matrix = []
+    headers = []
+
+    for i in range(Tablero_size[1]):
+        for l in index:
+            if not(questionare_matrix[l][i][0][0] in  headers):
+                headers.append(questionare_matrix[l][i][0][0])
+            answer_matrix.append(questionare_matrix[l][i][0][2:10])
+            question_matrix.append([questionare_matrix[l][i][0][1],questionare_matrix[l][i][0][10]])
+            
+
+    return question_matrix,answer_matrix,headers,errores
+
+
+def update_box(index,turn,position,BOXSIZE,WINDOW,teams):
+    _x = position[index][0]
+    _y =  position[index][1]
+
+    pg.draw.rect(WINDOW,teams[turn], pg.Rect(_x,_y,BOXSIZE[0],BOXSIZE[1]))
+    pg.display.flip()
+
+
+def click_in_box(position_matrix,mouse,BOXSIZE):
+    index = None
+    for i,position in enumerate(position_matrix):
+        in_range_x = (position[0] < mouse[0] and position[0] + BOXSIZE[0] > mouse[0])
+        in_range_y = (position[1] < mouse[1] and position[1] + BOXSIZE[1] > mouse[1])
+
+        hover = in_range_y and in_range_x
+        if hover:
+            index = i
+            break
+    return index
+
+def selection_screen(position,heders,BOXSIZE,tamaño_tablero,font,WINDOW):
+    WINDOW.fill(BLUE)
+    for i,element in enumerate(position):
+        pg.draw.rect(WINDOW,element[2], pg.Rect(element[0],element[1],BOXSIZE[0],BOXSIZE[1]))
+
+        puntos = str((i // tamaño_tablero[0])*100)
+        color = AQUA
+
+        if i<tamaño_tablero[0]:
+            puntos = heders[i]
+            color = BLACK
+
+        superficie = font.render(puntos,True,color)
+        text_rect = superficie.get_rect(center = (element[0]+BOXSIZE[0]//2,element[1]+BOXSIZE[1]//2))
+
+        WINDOW.blit(superficie,text_rect)
+        pg.display.flip()
+
+def question_screen(question,answes,size,font,WINDOW,window_size):
+    sub_area = [size[0],size[1] // 2]
+    _x = int(window_size[0] * 0.05)
+    _y = int(window_size[1] * 0.05)
+    WINDOW.fill(BLUE)
+    pg.draw.rect(WINDOW,RED, pg.Rect(_x,_y,sub_area[0],sub_area[1]))
+    superficie = font.render(question,True,BLACK)
+    text_rect = superficie.get_rect(center = (_x + sub_area[0] // 2,_y + sub_area[1] // 2))
+    WINDOW.blit(superficie,text_rect)
+
+    box_size = [sub_area[0] // 2,sub_area[1] // 2]
+    box_size = [box_size[0] - box_size[0] * 0.07,box_size[1] - box_size[1] * 0.07]
+
+    for num_box,text in enumerate(answes):
+        _x = int(window_size[0] * 0.05) + (int(box_size[0] * 0.159) + box_size[0]) * (num_box - 2 * (num_box // 2))
+        _y = int(window_size[1] * 0.06) + sub_area[1] + (int(box_size[1] * 0.1) + box_size[1]) * (num_box // 2)
+
+        pg.draw.rect(WINDOW,LIGHTBLUE, pg.Rect(_x,_y,box_size[0],box_size[1]))
+        superficie = font.render(answes[num_box],True,BLACK)
+        text_rect = superficie.get_rect(center = (_x+box_size[0]//2,_y+box_size[1]//2))
+        WINDOW.blit(superficie,text_rect)
+
+def answer_detect(mouse,truth_list,answes,size,WINDOW,font,window_size):
+    box_size = [size[0] // 2,size[1] // 4]
+    box_size = [box_size[0] - box_size[0] * 0.07,box_size[1] - box_size[1] * 0.07]
+
+    result = False
+    for num_box,_bool in enumerate(truth_list):
+        _x = int(window_size[0] * 0.05) + (int(box_size[0] * 0.159) + box_size[0]) * (num_box - 2 * (num_box // 2))
+        _y = int(window_size[1] * 0.06) + (size[1] // 2) + (int(box_size[1] * 0.1) + box_size[1]) * (num_box // 2)
+
+        in_range_x = (_x < mouse[0] and _x + box_size[0] > mouse[0])
+        in_range_y = (_y < mouse[1] and _y + box_size[1] > mouse[1])
+
+        hover = in_range_y and in_range_x
+        color = RED
+
+        if hover:
+            if (_bool.lower() == "true"):
+                color = GREEN
+                result = True
+
+            pg.draw.rect(WINDOW,color, pg.Rect(_x,_y,box_size[0],box_size[1]))
+            superficie = font.render(answes[num_box],True,BLACK)
+            text_rect = superficie.get_rect(center = (_x+box_size[0]//2,_y+box_size[1]//2))
+            WINDOW.blit(superficie,text_rect)
+            break
+
+    pg.display.update()
+    return result
+
+
+def popup(warning,type):
+    window = tk.Tk()
+
+    window.title(type)
+    #window.geometry("200x200")
+
+    color = "red"
+    if type == "Warning" :
+        color = "yellow"
+
+    tk.Label(window,text=type,bg=color).pack()
+    tk.Label(window,text="").pack()
+    for text in warning:
+        tk.Label(window,text=text).pack()
+
+    tk.Button(window,text=" OK ",command=window.destroy).pack(pady=20)
+
+    window.mainloop()
+
+    if type == "Warning" :
+        return True 
+    return False
+
+BLUE = (40,40,240)
+
+WHITE = (250,250,250)
+GREY = (125,125,125)
+BLACK = (0,0,0)
+
+RED = (250,40,40)
+ORANGE = (250,75,0)
+GREEN = (40,250,40)
+YELLOW = (250,250,40)
+PINK = (250,40,250)
+PURPLE = (125,0,125)
+AQUA = (0,150,135)
+BROWN = (110, 35, 25)
+LIGHTBLUE = (55, 210, 250)
--- game.py
+++ game.py
@@ -1,102 +1,77 @@
 import math as mth
 import pygame as pg
-import os
+import aux_funtions as af
 
 
 ALTURA = 720
 LARGO = 1080
-WINDOW = pg.display.set_mode((LARGO,ALTURA))
 FPS = 60
 BOXZONESIZE = [LARGO-int(LARGO*0.05)*2, ALTURA-int(ALTURA*0.05)*2]
-NUM_JUGADORES = 2
-PATH=os.getcwd()
-
-BLUE = (40,40,240)
-
-WHITE = (250,250,250)
-BLACK = (0,0,0)
-
-RED = (250,40,40)
-ORANGE = (250,75,0)
-GREEN = (40,250,40)
-YELLOW = (250,250,40)
-PINK = (250,40,250)
-PURPLE = (125,0,125)
-AQUA = (0,150,135)
-BROWN = (110, 35, 25)
-LIGHTBLUE = (55, 210, 250)
-
-teams = [WHITE,BLACK]
-
-COLORES = [BLUE,RED,ORANGE,YELLOW,GREEN,AQUA,LIGHTBLUE,PINK,PURPLE,BROWN]
-
-def sting_loder(Tablero_size):
-    question_matrix = []
-    answer_matrix = []
-    file = PATH + "/questions.txt"
-    file = open(file)
-    file = file.read()
-    for line in file:
-        text = line.split(" ")
-        question_matrix.append(text[0])
-        answer_matrix.append(text[1])
 
 
-def update_box(index,turn,position,BOXSIZE):
-    _x = position[index][0]
-    _y =  position[index][1]
 
-    pg.draw.rect(WINDOW,teams[turn], pg.Rect(_x,_y,BOXSIZE[0],BOXSIZE[1]))
-    pg.display.flip()
-
-
-def click_in_box(position_matrix,mouse,BOXSIZE):
-    index = None
-    for i,position in enumerate(position_matrix):
-        in_range_x = (position[0]< mouse[0] and position[0]+BOXSIZE[0] > mouse[0])
-        in_range_y = (position[1]< mouse[1] and position[1]+BOXSIZE[1] > mouse[1])
-
-        hover = in_range_y and in_range_x
-        if hover:
-            index = i
-            break
-    return index
-
-
-def main():
+def main(num_jugadores):
+    WINDOW = pg.display.set_mode((LARGO,ALTURA))
     
     runinng = True
 
     clock = pg.time.Clock()
-    TAMAÑO_TABLERO = [9,9]
-    BOXSIZE = [int(BOXZONESIZE[0] / TAMAÑO_TABLERO[0]),int(BOXZONESIZE[1] / TAMAÑO_TABLERO[1])]
+    TAMAÑO_TABLERO = [5,5]
+    BOXSIZE = [int(BOXZONESIZE[0] / TAMAÑO_TABLERO[0]),int(BOXZONESIZE[1] / (TAMAÑO_TABLERO[1]+1))]
     BOXSIZE = [BOXSIZE[0] - BOXSIZE[0] * 0.07,BOXSIZE[1] - BOXSIZE[1] * 0.07]  
-    Scores = [0]*NUM_JUGADORES
+    Scores = [0]*num_jugadores
+    pg.font.init() 
+    font = pg.font.SysFont('Arial', 30)
+    
 
     position_matrix = []
     discard_index = []
-    tablero = ["N/A"] * 9
-    for i in range(len(tablero)):
-        tablero[i] = [["N/A","N/A","N/A"],["N/A","N/A","N/A"],["N/A","N/A","N/A"]]
     
-    
+    question_matrix,answres_matrix,headers,warning = af.string_loder(TAMAÑO_TABLERO)
 
-    WINDOW.fill(BLUE)
+    popup_type = "Warning"
+    if question_matrix == "err":
+        popup_type = "Critical error"
+        warning.insert(0,"Juego no puede ejecutarse.")
+    
+    if len(warning)>0:
+        runinng = af.popup(warning,popup_type)
+
+    WINDOW.fill(af.BLUE)
+
+    for num_heder,heder in enumerate(headers):
+        _x = int(LARGO * 0.05) + (int(BOXSIZE[0] * 0.1) + BOXSIZE[0]) * (num_heder - TAMAÑO_TABLERO[0] * mth.floor(num_heder / TAMAÑO_TABLERO[0]))
+        _y = int(ALTURA * 0.03)
+
+        position_matrix.append([_x,_y,af.GREEN])
+
+        pg.draw.rect(WINDOW,af.GREEN, pg.Rect(_x,_y,BOXSIZE[0],BOXSIZE[1]))
+        superficie = font.render(heder,True,af.BLACK)
+        text_rect = superficie.get_rect(center = (_x+BOXSIZE[0]//2,_y+BOXSIZE[1]//2))
+        WINDOW.blit(superficie,text_rect)
+        pg.display.flip()
 
     for num_box in range(TAMAÑO_TABLERO[0] * TAMAÑO_TABLERO[1]):
         _x = int(LARGO * 0.05) + (int(BOXSIZE[0] * 0.1) + BOXSIZE[0]) * (num_box - TAMAÑO_TABLERO[0] * mth.floor(num_box / TAMAÑO_TABLERO[0]))
-        _y = int(ALTURA * 0.05) + (int(BOXSIZE[1] * 0.1) + BOXSIZE[1]) * mth.floor(num_box / TAMAÑO_TABLERO[0])
+        _y = int(ALTURA * 0.18) + (int(BOXSIZE[1] * 0.1) + BOXSIZE[1]) * mth.floor(num_box / TAMAÑO_TABLERO[0])
 
-        position_matrix.append([_x,_y])
-        colorid = mth.floor((mth.floor(num_box / 3) - 3 * mth.floor(num_box/TAMAÑO_TABLERO[0])) + 3 * mth.floor(num_box / (3 * TAMAÑO_TABLERO[0]))) + 1
-        color = COLORES[colorid]
+        position_matrix.append([_x,_y,af.RED])
 
-        pg.draw.rect(WINDOW,color, pg.Rect(_x,_y,BOXSIZE[0],BOXSIZE[1]))
+        pg.draw.rect(WINDOW,af.RED, pg.Rect(_x,_y,BOXSIZE[0],BOXSIZE[1]))
+        puntos = str(((num_box // TAMAÑO_TABLERO[0])+1)*100)
+        superficie = font.render(puntos,True,af.AQUA)
+        text_rect = superficie.get_rect(center = (_x+BOXSIZE[0]//2,_y+BOXSIZE[1]//2))
+        WINDOW.blit(superficie,text_rect)
         pg.display.flip()
 
 
     pg.display.update()
+
+    escena = 0
+    current_team = 0
     turn = 0
+    points = 0
+
     while runinng:
         clock.tick(FPS)
         draw = False
@@ -109,87 +84,80 @@ def main():
 
             elif event.type == pg.MOUSEBUTTONDOWN:
                 draw = True
-                pg.font.init() 
-                my_font = pg.font.SysFont('Comic Sans MS', 30)
                 mouse = pg.mouse.get_pos()
-
-                index = click_in_box(position_matrix,mouse,BOXSIZE)
                 
-                if index == None or (index in discard_index):
-                    break
+                if(escena == 0):
+                    index = af.click_in_box(position_matrix,mouse,BOXSIZE)
+                    
+                    if index == None or (index in discard_index):
+                        break
 
-                big_matrix = mth.floor(index / 3) - 3 * mth.floor(index/TAMAÑO_TABLERO[0]) + 3 * mth.floor(index / (3 * TAMAÑO_TABLERO[0]))
-                smol_matrix = [(index - TAMAÑO_TABLERO[0] * mth.floor(index / TAMAÑO_TABLERO[0]) - 3 * mth.floor((index - TAMAÑO_TABLERO[0] * mth.floor(index / TAMAÑO_TABLERO[0]))/3)),mth.floor(index / TAMAÑO_TABLERO[0]) - 3 * mth.floor((index / TAMAÑO_TABLERO[0]) / 3)]
-                
-                tablero[big_matrix][smol_matrix[0]][smol_matrix[1]] = turn
-                check = False
-                for l in range(7):
-                    if l < 3:
-                        check = ((tablero[big_matrix][l][0] == turn) and (tablero[big_matrix][l][2] == turn) and (tablero[big_matrix][l][1] == turn))
-                        if check:
-                            break
-                    elif l < 6:
-                        l = l-3
-                        check = ((tablero[big_matrix][0][l] == turn) and (tablero[big_matrix][2][l] == turn) and (tablero[big_matrix][1][l] == turn))
-                        if check:
-                            break
-                    else:
-                        check = ((tablero[big_matrix][0][0] == turn) and (tablero[big_matrix][2][2] == turn) and (tablero[big_matrix][1][1] == turn))
-                        if check:
-                            break
-                        check = ((tablero[big_matrix][0][2] == turn) and (tablero[big_matrix][2][0] == turn) and (tablero[big_matrix][1][1] == turn))
+                    discard_index.append(index)
+                    position_matrix[index][2] = af.GREY
+                    escena = abs(escena-1)
+                    points = int(question_matrix[index][1])
+                    af.question_screen(question_matrix[index][0],answres_matrix[index][:4],BOXZONESIZE,font,WINDOW,[LARGO,ALTURA])
+                    start_turn = turn
+                else:
+                    
+                    correct = af.answer_detect(mouse,answres_matrix[index][4:],answres_matrix[index][:4],BOXZONESIZE,WINDOW,font,[LARGO,ALTURA])
 
-                if check:
-                    for i in range(9):
-                        id = mth.floor(((i - 3 * mth.floor(i / 3)) + TAMAÑO_TABLERO[0] * mth.floor(i / 3)) + 3 * (big_matrix-3*mth.floor(big_matrix/3)) + (3 * TAMAÑO_TABLERO[0] * mth.floor(big_matrix / 3)))
-                        update_box(id,turn,position_matrix,BOXSIZE)
-                    tablero[big_matrix] = turn
+                    pg.time.wait(int(1000*0.5))
 
-                    for l in range(7):
-                        if l < 3:
-                            check = ((isinstance(tablero[l],int)) and (isinstance(tablero[l + 5],int)) and (isinstance(tablero[l + 2],int)))
-                            if check:
-                                check = ((tablero[l] == turn) and (tablero[l + 5] == turn) and (tablero[l + 2] == turn))
-                                if check:
-                                    break
-                        elif l < 6:
-                            j = l-2+2*(l-3)
-                            check = ((isinstance(tablero[j - 1],int)) and (isinstance(tablero[j + 1],int)) and (isinstance(tablero[j],int)))
-                            if check:
-                                check = ((tablero[j] == turn) and (tablero[j - 1] == turn) and (tablero[j + 1] == turn))
-                                if check:
-                                    break
-                        else:
-                            check = ((isinstance(tablero[0],int)) and (isinstance(tablero[7],int)) and (isinstance(tablero[4],int)))
-                            if check:
-                                check = ((tablero[0] == turn) and (tablero[7] == turn) and (tablero[4] == turn))
-                                if check:
-                                    break
-                            check = ((isinstance(tablero[2],int)) and (isinstance(tablero[6],int)) and (isinstance(tablero[4],int)))
-                            if check:
-                                check = ((tablero[2] == turn) and (tablero[6] == turn) and (tablero[4] == turn))
-                            
-                    if(check):
-                        tablero = turn
+                    if correct:
+                        escena = abs(escena-1) 
+                        af.selection_screen(position_matrix,headers,BOXSIZE,TAMAÑO_TABLERO,font,WINDOW)
+                        Scores[current_team] += points
+                    elif ((turn - start_turn) > 1):
+                        escena = abs(escena-1) 
+                        af.selection_screen(position_matrix,headers,BOXSIZE,TAMAÑO_TABLERO,font,WINDOW)
 
-                        
-                        
-                update_box(index,turn,position_matrix,BOXSIZE)
-
-                discard_index.append(index)
-                turn = abs(turn-1)
+                    turn += 1
+                    current_team = turn - num_jugadores * (turn // num_jugadores)
+                    
+                    
         
 
         if draw:
             pg.display.update()
         
-        if (isinstance(tablero,int)):
-            for i in range(len(position_matrix)):
-                update_box(i,abs(turn - 1),position_matrix,BOXSIZE)
-            pg.display.update()
+        if (len(discard_index) == TAMAÑO_TABLERO[0]*TAMAÑO_TABLERO[1]):
+            WINDOW.fill(af.BLUE)
+            new_box = [BOXZONESIZE[0]//2,BOXZONESIZE[1]//num_jugadores]
+            if new_box[1] > BOXSIZE[1]:
+                new_box[1] = BOXSIZE[1]
+            
+            for team in range(num_jugadores*2):
+                _x = int(LARGO * 0.05) + (int(BOXSIZE[0] * 0.1) + BOXSIZE[0]) * (num_box - 2 * mth.floor(num_box // 2))
+                _y = int(ALTURA * 0.18) + (int(BOXSIZE[1] * 0.1) + BOXSIZE[1]) * mth.floor(num_box // 2)
+
+                pg.draw.rect(WINDOW,af.RED, pg.Rect(_x,_y,BOXSIZE[0],BOXSIZE[1]))
+                puntos = str(((num_box // TAMAÑO_TABLERO[0])+1)*100)
+                superficie = font.render(puntos,True,af.AQUA)
+                text_rect = superficie.get_rect(center = (_x+BOXSIZE[0]//2,_y+BOXSIZE[1]//2))
+                WINDOW.blit(superficie,text_rect)
+                pg.display.flip()
+
 
     
     pg.quit()
 
 if __name__ == "__main__":
-    main()
+    num_jugadores = input("Numero de equipos: ")
+    if num_jugadores.isnumeric():
+        num_jugadores = int(num_jugadores)
+        if num_jugadores<2:
+            num_jugadores = 2
+            print("Numero de equipos tiene que ser mayor a 1.")
+    else:
+        num_jugadores = 2
+        print("Tiene que ser un numero mayor a 1.")
+    
+    print()
+    print("Numero de equipos: " + str(num_jugadores))
+    pg.time.wait(500)
+    print("Initializing...")
+    pg.time.wait(1000)
+
+    main(num_jugadores)
+    print("Done.")
